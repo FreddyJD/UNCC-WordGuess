3a50a8174e11ae7aaa61a09a4ae1b3df680f5b06
Freddy <freddy.jd2@gmail.com>
2019-01-29 10:16:44 -0500

    [🔥] Fixed the game logic

--- includes/game.js
+++ includes/game.js
@@ -1,23 +1,26 @@
 const words = [
-    "Bo Jack Horse man ",
-    "Stranger Things",
-    "House of Cards"];
+    "bojack",
+    "roma",
+    "lovesick",
+    "cuckoo"
+];
+
 let len = words.length;
 let choosed = words[Math.floor(Math.random() * len)].toLocaleLowerCase();
 var dword = choosed.split("");
 var hideWord = [];
-var maxWords = 15;
+var maxWords = 26;
 var score = 0;
 let tempA;
 let tempB;
 
+
 function start() {
-    console.log(dword);
-    for (let l = 0; l < dword.length; l++) {
-        if (dword[l] === " ") {
+    for (let i = 0; i < dword.length; i++) {
+        if (dword[i] === " ") {
             hideWord.push(" ");
         } else {
-            hideWord.push("_ ")[l];
+            hideWord.push(" _ ")[i];
             $('#netflix').html(hideWord);
         }
     }
@@ -25,33 +28,41 @@ function start() {
 start();
 
 document.addEventListener('keyup', (event) => {
+
     let temp = event.key.toLocaleLowerCase();
     tempA = dword.toString();
     tempB = hideWord.toString();
+
     maxWords--;
+    
     $('#maxWords').html(maxWords);
-    for (let l = 0; l < dword.length; l++) {
-        if (dword[l] === temp) {
-            hideWord[l] = temp
+    for (let i = 0; i < dword.length; i++) {
+        if (dword[i] === temp) {
+            hideWord[i] = temp
             $('#netflix').html(hideWord);
         }
     }
+    
     if (tempA === tempB) {
         score++;
         $('#score').html(score);
         restart();
+        
     } else if (maxWords === 0) {
         score--; 
         $('#score').html(score);
         restart();
     }
 });
+
+
+
 function restart() { 
     choosed = words[Math.floor(Math.random() * len)];
     dword = choosed.split("");
     hideWord = [];
     tempA = dword.toString();
     tempB = hideWord.toString();
-    maxWords = 15;
+    maxWords = 26;
     start();
 }
